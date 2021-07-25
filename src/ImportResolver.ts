7fcafdc22192f8d6b342ace17ccfe4d07e49e5bf
Marc <marcflament@free.fr>
2021-07-25 15:17:00 +0200

    Improved node_module file resolution

--- src/ImportResolver.ts
+++ src/ImportResolver.ts
@@ -1,10 +1,8 @@
-import { ResolvedFile } from './ResolvedFile';
-import { posix as path } from 'path';
-import { readFile, readFileSync } from 'fs';
-import { fileExists } from './Utils';
-import { MapLike } from 'typescript';
-import { ImportDeclaration } from './JSImportTransformer';
-import { CompilerOptions } from './Options';
+import {posix as path} from 'path';
+import {fileExists} from './Utils';
+import {ImportDeclaration} from './JSImportTransformer';
+import {CompilerOptions} from './Options';
+import {readFileSync} from "fs";
 
 export type ImportResolver = (declaration: ImportDeclaration) => string | undefined;
 
@@ -13,38 +11,47 @@ export function createImportResolver(options: CompilerOptions): ImportResolver {
 }
 
 function resolvePath(decl: ImportDeclaration, compilerOptions: CompilerOptions): string | undefined {
-  let resolvedPath;
+  let resolvedPath = resolveCompilerPaths(decl, compilerOptions);
+  if (!resolvedPath) resolvedPath = resolveNodeFile(decl);
+  if (!resolvedPath) resolvedPath = resolveNodePackage(decl);
+  return resolvedPath;
+}
+
+function resolveCompilerPaths(decl: ImportDeclaration, compilerOptions: CompilerOptions): string | undefined {
   let importPath = decl.path;
   for (const key in compilerOptions.paths) {
     const values = compilerOptions.paths[key];
     if (key === importPath) {
-      resolvedPath = values.find(v => fileExists(path.join(compilerOptions.outDir, v + '.js')));
-      break;
+      return values.find(v => fileExists(path.join(compilerOptions.outDir, v + '.js')));
     }
+
     const prefix = key.endsWith('*') ? key.substring(0, key.length - 1) : undefined;
     if (prefix && importPath.startsWith(prefix)) {
       const np = importPath.substring(prefix.length) + '.js';
-      resolvedPath = values.map(v => v.replace('*', np)).find(f => fileExists(path.resolve(compilerOptions.outDir, f)));
-      break;
+      return values.map(v => v.replace('*', np)).find(f => fileExists(path.resolve(compilerOptions.outDir, f)));
     }
   }
+  return undefined;
+}
 
-  if (resolvedPath) return resolvedPath;
+function resolveNodeFile(decl: ImportDeclaration): string | undefined {
+  const sourceFile = path.join('node_modules', decl.path + '.js');
+  if (fileExists(sourceFile))
+    return '/' + sourceFile;
+  return undefined;
+}
 
+function resolveNodePackage(decl: ImportDeclaration): string | undefined {
   const moduleName = decl.moduleName;
   const packageFile = path.join('node_modules', moduleName, 'package.json');
   if (fileExists(packageFile)) {
-    const npmPackage = JSON.parse(readFileSync(packageFile, { encoding: 'utf-8' }));
+    const npmPackage = JSON.parse(readFileSync(packageFile, {encoding: 'utf-8'}));
     let file = npmPackage['module'] || npmPackage['main'];
-    const projectPath = path.join('node_modules', moduleName, file);
-    if (typeof file === 'string' && fileExists(projectPath)) {
-      return '/' + projectPath;
+    if (typeof file === 'string') {
+      const projectPath = path.join('node_modules', moduleName, file);
+      if (fileExists(projectPath))
+        return '/' + projectPath;
     }
   }
-
-  return resolvedPath;
+  return undefined;
 }
-
-// function resolveNode(name: string): string | undefined {
-//   return undefined;
-// }
